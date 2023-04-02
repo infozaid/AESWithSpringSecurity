0ddcf7fc44fb4f06a6f46d22a05164de3c2d1693
infozaid <infozaidamin@gmail.com>
2023-04-02 18:06:23 +0500

    Spring Security Custom Implementation

---
+++ src/main/java/com/example/aeswithspringsecurity/util/refNoGenerator/UtilException.java
@@ -0,0 +1,12 @@
+package com.example.aeswithspringsecurity.util.refNoGenerator;
+
+import java.security.NoSuchAlgorithmException;
+
+
+public class UtilException extends RuntimeException{
+
+    public UtilException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
